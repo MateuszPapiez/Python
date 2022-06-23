af7c203a7938d96599dd0a40b8154dbe1ab02da0
Mateusz <56720382+MateuszPapiez@users.noreply.github.com>
2022-06-23 21:54:51 +0200

    new commit
    new commit

--- Api+QT/backend/main.py
+++ Api+QT/backend/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, Response
+from json import loads
 
 App=FastAPI()
 
@@ -13,3 +14,12 @@ def get_surveys():
 def get_survey(name):
     with open (f"{name}.json","r") as f:
         return Response(content=f.read())
+
+@App.post("/surveys/{name}")
+def post_answers(name,answers):
+    scores = {'spalanie':0,'aerodynamika':0}
+    with open (f"{name}_meta.json","r") as f:
+        meta_model=loads(f.read())
+    for a in answers:
+        category=meta_model[a.id]["category"]
+        change=meta_model[a.id][]
