c2298df751143a7cedc25f623195ce7a5d0ad397
Mateusz <56720382+MateuszPapiez@users.noreply.github.com>
2022-09-08 22:00:50 +0200

    add code
    add code

--- Api+QT 2/frontend/qtex.py
+++ Api+QT 2/frontend/qtex.py
@@ -11,6 +11,7 @@ from PyQt5 import QtCore
 from PyQt5 import QtGui
 from PyQt5.QtWidgets import QCheckBox
 from PyQt5.QtGui import QIcon
+from flutter import checked
 import requests
 
 
@@ -20,6 +21,49 @@ SURVEY_QUESTIONS="http://127.0.0.1:5555/surveys"
 app=None
 window=None
 
+class QuestionPanel (QWidget):
+
+   def __init__(self,question,parent,callback=None):
+      super().__init__(parent)
+      self.Label=QLabel(question['pytanie'],self)
+      self.Label.setGeometry(0,0,120,20)
+      self.Label.setStyleSheet("QLabel { color : blue; }")
+      self.C1=QCheckBox(self)
+      self.C1.stateChanged.connect(lambda : self.uncheck("A"))
+      self.C1.setGeometry(0,20,120,20)
+      self.C1.setText(question['odpowiedzi'][0]['odpowiedz'])
+      
+
+      self.C2=QCheckBox(self)
+      self.C2.stateChanged.connect(lambda : self.uncheck("B"))
+      self.C2.setGeometry(0,40,120,20)
+      self.C2.setText(question['odpowiedzi'][1]['odpowiedz'])
+      self.C3=QCheckBox(self)
+      self.C3.stateChanged.connect(lambda : self.uncheck("C"))
+      self.C3.setGeometry(0,60,120,20)
+      self.C3.setText(question['odpowiedzi'][2]['odpowiedz'])
+      
+   
+   uncheck_running=False
+   
+   def uncheck(self,e):
+      if self.uncheck_running:
+         return
+      self.uncheck_running=True
+      if e=="A":
+         self.C1.setCheckState(1)
+         self.C2.setCheckState(0)
+         self.C3.setCheckState(0)
+      if e=="B":
+         self.C2.setCheckState(1)
+         self.C1.setCheckState(0)
+         self.C3.setCheckState(0)
+      if e=="C":
+         self.C3.setCheckState(1)
+         self.C1.setCheckState(0)
+         self.C2.setCheckState(0)
+      self.uncheck_running=False
+      
 
 def button(survey_name,parent,go_to_survey_action):
    button1 = QPushButton(parent)
@@ -28,41 +72,40 @@ def button(survey_name,parent,go_to_survey_action):
    button1.clicked.connect(lambda : go_to_survey_action(survey_name))
    return button1
 
-def question_panel(question,parent,callback=None):
-   panel= QWidget(parent)
-   Label=QLabel(question['pytanie'],panel)
-   Label.setGeometry(0,0,120,80)
-   Label.setStyleSheet("QLabel { background-color : red; color : blue; }")
-   C1=QCheckBox(panel)
-   C2=QCheckBox(panel)
-   C3=QCheckBox(panel)
-   return panel
 
+   
 
+class Surveywindow (QWidget):
 
-def survey_window(name):
+  def __init__(self,name):
+
+   
    survey=requests.get(SURVEY_QUESTIONS+"/"+name).json()
    survey_panel=QWidget(window)
    y=0
 
    survey_panel.setGeometry(0,0,800,400)
    survey_panel.setStyleSheet("QWidget { background-color : grey; color : white; }")
-
-   for question in survey:
       
-      panel=question_panel(question,survey_panel)
-      panel.setGeometry(300,y,120,300)
+   for question in survey:
+         
+      panel=QuestionPanel(question,survey_panel)
+      panel.setGeometry(300,y,250,300)
       panel.show()
       y+=100
       
+      
    survey_panel.show()
+
+
+def 
 
 def init_window():
    global app
    global window
    app = QApplication(sys.argv)
    window = QWidget()
-   #window.setLayout(QGridLayout())
+
    
    window.setGeometry(800,200,750,400)
    window.setWindowTitle("Ankiety")
@@ -75,6 +118,7 @@ def init_window():
       survey_button=button(name,window,survey_window)
       survey_button.setGeometry(300,button_y,150,80)
       button_y+=100
+      
 
 
 
